b7542efaec9db6b0eb0c4bbca36d67a49c6bafbf
Noah-Wu66 <noah.wu66@gmail.com>
2025-12-19 17:43:59 +0800

    feat: 添加 Tailwind CSS 开发依赖并调整其配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,9 @@ module.exports = {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {},
-        container: {
-            center: true,
-            padding: "2rem",
-            screens: {
-                "2xl": "1400px",
-            },
-        },
     },
     plugins: [],
 };
